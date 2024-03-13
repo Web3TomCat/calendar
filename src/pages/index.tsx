4ac3b4c79c30fbefb8b68162ec9d0c4a9bc08d94
李法师 <liujiantao86@foxmail.com>
2024-03-13 11:41:32 +0800

    firebase

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,17 +9,47 @@ import Script from "next/script";
 
 import { initializeGA } from "@/utilities/google-analytics";
 
+
+
+
+import { initializeApp } from 'firebase/app';
+import { getAnalytics, logEvent } from "firebase/analytics";
+// For Firebase JavaScript SDK v7.20.0 and later, `measurementId` is an optional field
+const firebaseConfig = {
+  apiKey: "AIzaSyCGQ0tYppWFJkuSxBhOpkH0xVDmX245Vdc",
+  authDomain: "project-id.firebaseapp.com",
+  databaseURL: "https://project-id.firebaseio.com",
+  projectId: "project-id",
+  storageBucket: "project-id.appspot.com",
+  messagingSenderId: "637908496727",
+  appId: "2:637908496727:web:a4284b4c99e329d5",
+  measurementId: "G-9VP01NDSXJ"
+};
+
+
+
+
 export default function Home() {
 
 
+  // useEffect(() => {
+  //   // @ts-ignore
+  //   if (!window.GA_INITIALIZED) {
+  //     initializeGA();
+  //     // @ts-ignore
+  //     window.GA_INITIALIZED = true;
+  //   }
+  // }, []);
+
+
   useEffect(() => {
-    // @ts-ignore
-    if (!window.GA_INITIALIZED) {
-      initializeGA();
-      // @ts-ignore
-      window.GA_INITIALIZED = true;
-    }
-  }, []);
+    const app = initializeApp(firebaseConfig);
+    console.log('app', app)
+    const analytics = getAnalytics(app);
+    console.log('analytics', analytics)
+    // logEvent(analytics, 'notification_received');
+
+  },[])
 
   return (
     <div >
