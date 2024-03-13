15e28a7885a86b650c283befad1433e72155368b
李法师 <liujiantao86@foxmail.com>
2024-03-13 11:45:02 +0800

    change

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -16,14 +16,13 @@ import { initializeApp } from 'firebase/app';
 import { getAnalytics, logEvent } from "firebase/analytics";
 // For Firebase JavaScript SDK v7.20.0 and later, `measurementId` is an optional field
 const firebaseConfig = {
-  apiKey: "AIzaSyCGQ0tYppWFJkuSxBhOpkH0xVDmX245Vdc",
-  authDomain: "project-id.firebaseapp.com",
-  databaseURL: "https://project-id.firebaseio.com",
-  projectId: "project-id",
-  storageBucket: "project-id.appspot.com",
-  messagingSenderId: "637908496727",
-  appId: "2:637908496727:web:a4284b4c99e329d5",
-  measurementId: "G-9VP01NDSXJ"
+  apiKey: "AIzaSyBix0_l0V2BRG8VCuTid97j7yRHIzQtUUA",
+  authDomain: "predx-46a86.firebaseapp.com",
+  projectId: "predx-46a86",
+  storageBucket: "predx-46a86.appspot.com",
+  messagingSenderId: "580898390555",
+  appId: "1:580898390555:web:72df2516f0ea7a3b412ebe",
+  measurementId: "G-95F3FKJH3Q"
 };
 
 
